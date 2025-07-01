e52d4378acf24bad27703c693f1de83fb5db2d2d
Parth Patidar <parth1207patidar@gmail.com>
2025-07-01 20:02:55 +0530

    chore(signup): added redirection after successful signup

--- src/hooks/auth/use-signup.ts
+++ src/hooks/auth/use-signup.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { signUpWithEmail } from "@/services/auth/auth.services";
 import { createUserProfile } from "@/services/auth/profile.services";
+import { queryClient } from "@/lib/tanstack/client";
+import { useNavigate } from "react-router-dom";
 
 type SignUpPayload = {
   name: string;
@@ -10,11 +12,16 @@ type SignUpPayload = {
 };
 
 const useSignUp = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: async ({ name, email, password, role }: SignUpPayload) => {
       const { user } = await signUpWithEmail(email, password);
       if (!user) throw new Error("Failed to sign up");
       await createUserProfile({ id: user.id, name, email, role });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      navigate("/login");
     },
   });
 };
