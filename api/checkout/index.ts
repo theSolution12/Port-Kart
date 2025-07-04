711c30b0ebc73623d7f321e5634036d06e6360d4
Parth Patidar <parth1207patidar@gmail.com>
2025-07-04 09:11:40 +0530

    debug(checkout): debugging api for permanent code 500 error.

--- api/checkout/index.ts
+++ api/checkout/index.ts
@@ -78,6 +78,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     return res.status(200).json({ success: true });
   } catch (error: any) {
+    console.error(error);
     return res.status(500).json({ error: error.message });
   }
 }
